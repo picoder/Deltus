1da35fc9a27b2e56d33604614c450b6762a4f011
Krystian Thiede BOSS <thiede@promo-expo.pl>
2012-01-05 02:51:34 +0100

    TTP theme continue (main site html ready)

--- themes/ttp_theme/controllers/ttp_theme.php
+++ themes/ttp_theme/controllers/ttp_theme.php
@@ -13,12 +13,21 @@ class Ttp_theme extends DV_Controller
 		$this->theme->set_theme_css('ttp_theme', 'v');
 		$this->theme->set_theme_css('ttp_theme', 'base');
 		$this->theme->set_theme_css('ttp_theme', 'style');
+		$this->theme->set_theme_css('ttp_theme', 'orbit');
 		
 		$this->theme->set_theme_js('ttp_theme', 'jquery-1.6.4.min');
 		$this->theme->set_theme_js('ttp_theme', 'jquery.easing.1.3');
 		$this->theme->set_theme_js('ttp_theme', 'smile');
+		$this->theme->set_theme_js('ttp_theme', 'jquery.orbit-1.2.3.min');
 		
 		$this->theme->set_full("<link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>");
+		$this->theme->set_full("<link href='http://fonts.googleapis.com/css?family=Marck+Script&subset=latin-ext' rel='stylesheet' type='text/css'>");
+		
+		$this->theme->set_full("<script type='text/javascript'>
+			$(window).load(function() {
+				$('#featured').orbit({timer:true, captions:true, advanceSpeed:5000, animationSpeed:800, animation: 'fade', captionAnimationSpeed:800, captionAnimation: 'slideOpen', pauseOnHover: false});
+			});
+		</script>");
 		
 		$this->load->view('ttp_theme/ttp_theme');
 	}
--- themes/ttp_theme/views/ttp_theme.php
+++ themes/ttp_theme/views/ttp_theme.php
@@ -8,28 +8,25 @@ echo $this->theme->get_head('ttp_theme');
 </div><!--div.bg_wrapper-->
 
 <div class="w wrapper no_hidden">
-<div class="sticker">
+<div class="sticker"></div>
+<div class="sticker_red"></div>
 
-</div>
-<div class="sticker_red">
-
-</div>
-</div>
+</div> <!-- div.no_hidden -->
 
 
 <div class="w wrapper">
-<div class="v bg_row1"></div>
+
 <div class="v top">
 <div class="inner_top">
-<div class="l top_left1">
-<div class="logo"></div> 
-</div>
-<div class="l top_left2">
-<h1 class="t_center">Trójmiejskie Targi Pracy <br><span class="t_red">23-24 kwietnia 2012</span><br><span class="t_yel">
-WETI, Politechnika Gdańska</span></h1>
+<div class="l top_left">
+<div class="logo"></div>
+
 </div>
 <div class="l top_mid">
-
+<h1 class="ozd">&#8222;Budujemy przestrzeń... dla pracodawców i magistrów. <br>Mamy dużo pracy... do zaoferowania&#8221;<br><span class="t_red">Politechnika Gdańska, </span><span class="t_green"> 23-24 kwietnia 2012</span></h1>
+</div><!-- div.top_mid -->
+<div class="e o top_right">
+<div class="inner_top_right v">
 <div class="round">
 
             <div class="round_c v">
@@ -53,6 +50,19 @@ WETI, Politechnika Gdańska</span></h1>
                     </li>
                          
                     </ul>
+                    <ul class="lang_list">
+                    <li>
+                        <a href="#" target="_self">
+                        	<img alt="Wersja polska" src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/template/lang_pol.png">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" target="_self">
+                        	<img alt="Wersja angielska" src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/template/lang_eng.png">
+                        </a>
+                    </li>
+                         
+                    </ul>
 				</div> 
             </div><!-- div.round_c -->
             
@@ -63,10 +73,14 @@ WETI, Politechnika Gdańska</span></h1>
         </div>
    
 </div><!--div.round-->
-
-</div><!-- div.top_mid -->
-<div class="e o top_right">
-
+</div><!--div.inner_top_right-->
+<div class="inner_top_right_2 v">
+<p>
+<strong>Partner Strategiczny</strong><br>
+<a href="#" target="_self">
+<img alt="WETI PG" src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/template/eti_logo.gif">
+</a>
+</div><!--div.inner_top_right_2-->
 </div>
 
 </div><!-- div.inner_top -->
@@ -84,47 +98,111 @@ WETI, Politechnika Gdańska</span></h1>
                         <li><a href="#"><span>24 kwietnia</span></a></li>
                     </ul>
                 </li>
-                <li><a href="#"><span>Prezentacje</span></a></li>
+                <li><a href="#"><span>Prezentacje</span></a>
+                	<ul>
+                    	<li><a href="#"><span>23 kwietnia</span></a></li>
+                        <li><a href="#"><span>24 kwietnia</span></a></li>
+                    </ul>
+          </li>
                 <li><a href="#"><span>Oferta</span></a></li>
                 <li><a href="#"><span>Ogłoszenia</span></a></li>
                 <li><a href="#"><span>Partnerzy</span></a></li>
                 <li><a href="#"><span>Kontakt</span></a></li>
             </ul>
         </div>
-    </div>
+    <div id="featured"> 
+     <img src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/dummy-images/menu_bg.jpg" alt="Overflow: Hidden No More" data-caption="#htmlCaption1">
+     <img src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/dummy-images/menu_bg_2.jpg"  alt="HTML Captions" data-caption="#htmlCaption2">
+     <img src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/dummy-images/suhner.jpg"  alt="Huber-Suhner" data-caption="#htmlSuhner">
+     <img src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/dummy-images/sii.jpg"  alt="Sii" data-caption="#htmlSii">
+	</div>  
+    <span class="orbit-caption" id="htmlCaption1">Trójmiejskie Targi Pracy 2012 | 23-24 kwietnia | Politechnika Gdańska</span>
+  	<span class="orbit-caption" id="htmlCaption2">Ponad 50 wystawców | Ponad 1000 ofert pracy | 10 000 odwiedzających</span>
+    <span class="orbit-caption" id="htmlSuhner">Wystawcy - Huber-Suhner</span>
+     <span class="orbit-caption" id="htmlSii">Wystawcy - Sii</span>
+    
+    </div><!-- div.inner-nav  -->
+    
 </div><!-- div.nav -->
 
 <div class="v content">
-<div class="l col_left">
-COL LEFT
-</div>
-<div class="r col_right">
-COL RIGHT
-</div>
-<div class="e o col_mid">
-<div class="col_mid_inner">
+<div class="l col_left"><a href="#" title="Rezerwacja stoisk">
+<div class="inner_col_content shadow">
+<h2><img alt="Zapraszamy" src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/template/icons/key.png">Zarezerwuj stoisko</h2>
+<p class="spirit">
+W dniach od 23 do 24 kwietnia 2012 roku mamy zaszczyt zaprosić Państwa do wzięcia udziału w kolejnej edycji Trójmiejskich Targów Pracy odbywających się w Nowym Gmachu Wydziału Elektroniki, Telekomunikacji i Informatyki Politechniki Gdańskiej.
+<br>
+Kontynuując nieprzerwaną od 1996 roku tradycję Targów Pracy organizowanych na Politechnice Gdańskiej tworzymy z pasją i poświęceniem dla Państwa Trójmiejskie Targi Pracy. Największe tego typu przedsięwzięcie w Polsce, które swoim zasięgiem będzie skupiać zainteresowanych zarówno z całego Pomorza, kraju, jak i zagranicy.
+</p>
+</div></a>
+</div><!--div.col_left-->
+<div class="r col_right"><a href="#" title="Stanowiska - Trtójmiejskie Targi Pracy 2012">
+<div class="inner_col_content shadow">
+<h2><img alt="Zapraszamy" src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/template/icons/map.png">Mapa stoisk</h2>
+<p class="spirit">
+W dniach od 23 do 24 kwietnia 2012 roku mamy zaszczyt zaprosić Państwa do wzięcia udziału w kolejnej edycji Trójmiejskich Targów Pracy odbywających się w Nowym Gmachu Wydziału Elektroniki, Telekomunikacji i Informatyki Politechniki Gdańskiej.
+<br>
+Kontynuując nieprzerwaną od 1996 roku tradycję Targów Pracy organizowanych na Politechnice Gdańskiej tworzymy z pasją i poświęceniem dla Państwa Trójmiejskie Targi Pracy. Największe tego typu przedsięwzięcie w Polsce, które swoim zasięgiem będzie skupiać zainteresowanych zarówno z całego Pomorza, kraju, jak i zagranicy.
+</p>
+</div></a>
+</div><!--div.col_right-->
+<div class="e o col_mid"><a href="#" title="Witamy poszukujących pracy">
+<div class="inner_col_content shadow">
+<h2><img alt="Zapraszamy" src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/template/icons/home.png">Zapraszamy</h2>
+<p class="spirit">
 
-COL MID
+W dniach od 23 do 24 kwietnia 2012 roku mamy zaszczyt zaprosić Państwa do wzięcia udziału w kolejnej edycji Trójmiejskich Targów Pracy odbywających się w Nowym Gmachu Wydziału Elektroniki, Telekomunikacji i Informatyki Politechniki Gdańskiej.
+<br>
+Kontynuując nieprzerwaną od 1996 roku tradycję Targów Pracy organizowanych na Politechnice Gdańskiej tworzymy z pasją i poświęceniem dla Państwa Trójmiejskie Targi Pracy. Największe tego typu przedsięwzięcie w Polsce, które swoim zasięgiem będzie skupiać zainteresowanych zarówno z całego Pomorza, kraju, jak i zagranicy.
+</p>
+</div></a>
 
-</div>
 </div><!-- div.col_mid -->
 </div><!--div.content-->
 
 <div class="v aside">
-
-ASIDE
-
+<div class="inner_aside shadow">
+<p>
+Zapraszamy odwiedzających do zapisywania się na na prezentacje i warsztaty firm uczestników Trójmiejskich Targów Pracy 2012. <a href="#" title="Rozmowy kwalifikacyjne, Prezentacje, Warsztaty - Trtójmiejskie Targi Pracy 2012">Więcej...</a>
+</p>
 </div>
+</div><!--div.aside-->
 
 <div class="v media">
 
-MEDIA
+<h3>Partnerzy medialni</h3>
+<div class="media_partners_table glow">
+<table>
+<tr>
+    <td>
+        <a href="#" target="_self">
+        	<img alt="Wirtualna Polska" src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/logos/medialni/wp.jpg">
+        </a>
+    </td>
+    <td>
+        <a href="#" target="_self">
+        	<img alt="Trojmiasto.pl" src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/logos/medialni/trojmiasto.jpg">
+        </a>
+    </td>
+    <td>
+        <a href="#" target="_self">
+        	<img alt="Anonse" src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/logos/medialni/anonse.jpg">
+        </a>
+    </td>
+    <td>
+        <a href="#" target="_self">
+        	<img alt="Studentnews" src="http://localhost/Deltus/themes/ttp_theme/views/assets/images/logos/medialni/student_news.jpg">
+        </a>
+    </td>
+</tr>
 
+</table>
+</div><!--div.media_partners_table-->
 </div>
 
 <div class="v footer">
 
-FOOTER
+<h4><strong>&copy;Trójmiejskie Targi Pracy 2012, biuro@targipracy.gdansk.pl, Polska<?php echo strftime("%Y"); ?> | TTP All rights reserved | Powered by PICODERS.pl</strong></h4>
 
 </div>
 
