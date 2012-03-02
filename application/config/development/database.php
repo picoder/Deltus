f037ed002f4527840b1fe879d7f82ab602305b2c
Krystian Thiede BOSS <thiede@promo-expo.pl>
2012-03-02 19:28:40 +0100

    Prepare database for work on Boss.

--- application/config/development/database.php
+++ application/config/development/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['username'] = 'namurzyn_deltus';
+$db['default']['password'] = 'namurzyn_deltus';
 $db['default']['database'] = 'namurzyn_deltus_chris_users';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
